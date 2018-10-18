c0a43b2a4a151144dcaa2cf29f0f27b62c1f33e7
Conormc98 <43957709+Conormc98@users.noreply.github.com>
2018-10-18 11:30:14 +0100

    Update express

--- Express/index.js
+++ Express/index.js
@@ -10,27 +10,14 @@ function updateEmployees(employeesReady) {
     });
 };
 
-function updateEmployeesDept(employeesReady) {
-    db.getEmployeeDept(function(rows) {
-        employees = rows;
-        employeesReady();
-    });
-};
-
 employees = [];
 
 app.get("/get-Employees", function(req, res) {
     updateEmployees(function() {
       res.send(employees);
     });  
-});
+  });
 
-app.get("/get-Employee-Dept", function(req, res) {
-    updateEmployeesDept(function() {
-        res.send(employees);
-    })
-})
-
-app.listen(8002, function() {
+app.listen(8010, function() {
     console.log("Express started");
 });
