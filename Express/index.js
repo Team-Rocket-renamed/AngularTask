af749895c680825597b12fbfbe76667d58692694
e-mccoy <44257526+e-mccoy@users.noreply.github.com>
2018-10-18 09:56:26 +0100

    first spike express

--- Express/index.js
+++ Express/index.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const db = require("./db.js");
+
+function updateEmployees(employeesReady) {
+    db.getEmployee(function(rows) {
+        employees = rows;
+        employeesReady();
+    });
+};
+
+employees = [];
+
+app.get("/employees", function(req, res) {
+    updateEmployees(function() {
+      res.send(employees);
+    });  
+  });
+
+app.listen(8002, function() {
+    console.log("Express started");
+});
